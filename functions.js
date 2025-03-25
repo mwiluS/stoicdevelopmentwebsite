6ccd90b9daa888c9afea8e8e2719bb075f065f21
Mwilu Siakachoma <111108840+mwiluS@users.noreply.github.com>
2025-03-25 18:09:59 -0400

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,9 @@
+function showSideBar(){
+    const sidebar = document.querySelector('.sideBar')
+    sidebar.style.display = 'flex'
+}
+
+function hideSideBar(){
+    const sidebar = document.querySelector('.sideBar')
+    sidebar.style.display = 'none'
+}
